69315c43217e3b849b95df6fff96c20ddad59b43
vicente almeida <vicente.cra45@gmail.com>
2024-01-14 19:53:15 +0400

    Adding dinamic mappin od pin to the map, based on size

--- src/components/PartnersMap.tsx
+++ src/components/PartnersMap.tsx
@@ -1,29 +1,51 @@
-import { useRef } from 'react';
+import { useRef, useState, type Key, useEffect } from 'react';
 import { useIntersectionObserver } from '../lib/Hooks/useIntersectionObserver';
+import { useMeasure } from '../lib/Hooks/useMeasure';
+import { partners, type Partner } from '@/consts';
 
-const georgiaPartnersImage = "/georgia-map-partners.png";
+// const georgiaPartnersImage = "/georgia-map-partners.png";
 const mapPin = "map-pin.png";
 
-const PartnersMap = () => {
+type Props = {
+  georgiaPartnersImage : string;
+};
+
+const PartnersMap = (props : Props) => {
+  const { georgiaPartnersImage } = props;
   const ref = useRef<HTMLDivElement | null>(null)
+  const [refImage, { height, offsetLeft, offsetTop, width }] = useMeasure();
+  console.log("width: ", offsetLeft, offsetTop);
   const entry = useIntersectionObserver(ref, {
     freezeOnceVisible: true,
   })
   const isVisible = !!entry?.isIntersecting
+
   return (
     <div className="relative">
         <div ref={ref} className="flex items-center justify-center bg-center bg-cover">
-            {georgiaPartnersImage && <img draggable="false" src={georgiaPartnersImage} alt=""/>}  
+            {georgiaPartnersImage && <img ref={refImage} draggable="false" src={georgiaPartnersImage} alt=""/>}  
         </div>
         {isVisible && (
-            <>
-              <div className="absolute inset-0 z-10 flex items-center justify-center animate-pin"> 
-                {mapPin && <img draggable="false" src={mapPin} alt="" width="32" height="32"/>}  
-              </div>
-              <div className="absolute inset-0 z-10 flex items-center justify-center ml-64 mt-16 animate-pin"> 
-                {mapPin && <img draggable="false" src={mapPin} alt="" width="32" height="32"/>}  
-              </div>
-            </>
+          partners.map((pin: Partner) => (
+            <div
+              key={pin.id}
+              className="absolute z-10 animate-pin"
+              style={{
+                left: offsetLeft + ((pin.x / 100) * width),
+                top: offsetTop + ((pin.y / 100) * height),
+              }}
+            >
+              <img draggable="false" src={mapPin} alt="" width="32" height="32" />
+            </div>
+          ))
+            // <>
+            //   <div className="absolute inset-0 z-10 flex items-center justify-center animate-pin"> 
+            //     {mapPin && <img draggable="false" src={mapPin} alt="" width="32" height="32"/>}  
+            //   </div>
+            //   <div className="absolute inset-0 z-10 flex items-center justify-center ml-64 mt-16 animate-pin"> 
+            //     {mapPin && <img draggable="false" src={mapPin} alt="" width="32" height="32"/>}  
+            //   </div>
+            // </>
         )}
     </div>
   )
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,40 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export type Partner = {
+    id: number;
+    name: string;
+    logo: string;
+    x: number;
+    y: number;
+};
+
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+
+export const SITE_URL = 'https://astro-blog.netlify.app';
+
+export const partners : Partner[] = [
+    {
+        id: 1,
+        name: 'Tbilisi Shop',
+        logo: '/images/astro.png',
+        x: 67,
+        y: 53,
+    },
+    {
+        id: 2,
+        name: 'Kakheti Shop',
+        logo: '/images/blog.png',
+        x: 75,
+        y: 45,
+    },
+    {
+        id: 3,
+        name: 'Batumi Shop',
+        logo: '/images/netlify.png',
+        x: 27,
+        y: 49
+    }
+];
