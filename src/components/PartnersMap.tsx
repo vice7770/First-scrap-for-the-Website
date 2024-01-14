f34c19fe88899cdf602622a908eec76d88f6a722
vicente almeida <vicente.cra45@gmail.com>
2024-01-14 13:47:04 +0400

    Adding react hook for interactivity

--- src/components/PartnersMap.tsx
+++ src/components/PartnersMap.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { useIntersectionObserver } from '../lib/Hooks/useIntersectionObserver';
+
 const georgiaPartnersImage = "/georgia-map-partners.png";
 const mapPin = "map-pin.png";
 
 const PartnersMap = () => {
   const [imageLoaded, setImageLoaded] = useState(false);
+  console.log("this is the component");
+  const ref = useRef<HTMLDivElement | null>(null)
+  const entry = useIntersectionObserver(ref, {
+    freezeOnceVisible: true,
+  })
+  const isVisible = !!entry?.isIntersecting
   return (
     <div className="relative">
-        <div className="flex items-center justify-center bg-center bg-cover">
-            {georgiaPartnersImage && <img draggable="false" src={georgiaPartnersImage} alt="" onLoad={() => setImageLoaded(true)}/>}  
+        <div ref={ref} className="flex items-center justify-center bg-center bg-cover">
+            {georgiaPartnersImage && <img draggable="false" src={georgiaPartnersImage} alt=""/>}  
         </div>
-        {imageLoaded && (
+        {isVisible && (
             <>
               <div className="absolute inset-0 z-10 flex items-center justify-center animate-pin"> 
                 {mapPin && <img draggable="false" src={mapPin} alt="" width="32" height="32"/>}  
