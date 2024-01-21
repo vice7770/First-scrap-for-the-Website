efeba862aac7e2707ac1353cfe43e3fe0ec649cc
vicente almeida <vicente.cra45@gmail.com>
2024-01-21 14:35:41 +0400

    Installing Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import react from "@astrojs/react";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [tailwind({
-      config: {
-        applyBaseStyles: false
-      }
-    }), 
-    mdx(), sitemap(), react()
-  ]
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx(), sitemap(), react()],
+  output: "server",
+  adapter: vercel()
 });
