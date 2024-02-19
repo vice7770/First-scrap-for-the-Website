52b2968f637edf9615e5ad654c9f37b17dc2784f
vicente almeida <vicente.cra45@gmail.com>
2024-02-19 20:07:18 +0400

    Adding Sign out on supabase

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,7 +1,15 @@
+import { supabase } from "@/lib/supabase";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
   cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" });
+  
+  const { error } = await supabase.auth.signOut()
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
+
   return redirect("/signin");
 };
