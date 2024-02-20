b063f4b5a7e91f685e31a1f5e26406834393268f
vicente almeida <vicente.cra45@gmail.com>
2024-02-20 21:51:01 +0400

    changing item params in addCart to be more clear

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -2,19 +2,13 @@ import React, { useEffect, useState } from "react";
 import {Button} from "@/components/ui/button";
 import { addCartItemOffline } from "@/stores/cart";
 
-async function addToCart({id, quantity, price, imageUrl, name}: {id: string, quantity: number, price: number, imageUrl: string, name: string}) {
-    addCartItemOffline({
-        id: id,
-        price: price,
-        quantity: quantity,
-        imageUrl: imageUrl,
-        name: name
-    });
+async function addToCart(item: {id: string, quantity: number, price: number, imageUrl: string, name: string}) {
+    addCartItemOffline(item);
 }
 
 export default function AddToCartButton({id, quantity, price, imageUrl, name}: {id: string, quantity: number, price: number, imageUrl: string, name: string}) {
     const [isLoading, setIsLoading] = useState(true);
-
+    const item = {id, quantity, price, imageUrl, name};
     useEffect(() => {
         setIsLoading(false);
     }, []);
@@ -24,7 +18,7 @@ export default function AddToCartButton({id, quantity, price, imageUrl, name}: {
             <Button className="bg-blue-500 text-white text-lg px-4 py-2 rounded-3xl mb-2 w-[250px] h-[50px]">Add to cart</Button>
         )
     }
-    return <Button className="bg-blue-500 text-white text-lg px-4 py-2 rounded-3xl mb-2 w-[250px] h-[50px]" onClick={() => addToCart({id, quantity, price, imageUrl, name})}>Add to cart</Button>;
+    return <Button className="bg-blue-500 text-white text-lg px-4 py-2 rounded-3xl mb-2 w-[250px] h-[50px]" onClick={() => addToCart(item)}>Add to cart</Button>;
 }
 
 
