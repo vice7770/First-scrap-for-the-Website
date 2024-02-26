657fd6353309a83da796090a32a33c82f9dea0f1
vicente almeida <vicente.cra45@gmail.com>
2024-02-26 15:21:46 +0400

    Middleware refactoring

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,36 +1,61 @@
 import { defineMiddleware } from "astro:middleware";
 import { supabase } from "../lib/supabase";
 import micromatch from "micromatch";
+import type { Session, User } from "@supabase/supabase-js";
+import type { AstroCookies } from "astro";
 
 const protectedRoutes = ["/profile(|/)", "/billing(|/)"];
 const redirectRoutes = ["/signin(|/)"];
 
+type data = {
+  user: User | null;
+  session: Session | null;
+}
+
+function cleanCookies(cookies : AstroCookies) {
+  // const cookies = document.cookie.split(";");
+  // for (let i = 0; i < cookies.length; i++) {
+  //   const cookie = cookies[i];
+  //   const eqPos = cookie.indexOf("=");
+  //   const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+  //   document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  // }
+  cookies.delete("sb-access-token", {
+    path: "/",
+  });
+  cookies.delete("sb-refresh-token", {
+    path: "/",
+  });
+}
+
 export const onRequest = defineMiddleware(
   async ({ locals, url, cookies, redirect }, next) => {
-    if (micromatch.isMatch(url.pathname, protectedRoutes)) {
-      const accessToken = cookies.get("sb-access-token");
-      const refreshToken = cookies.get("sb-refresh-token");
+    console.log(url.pathname);
+    const accessToken = cookies.get("sb-access-token");
+    const refreshToken = cookies.get("sb-refresh-token");
 
-      if (!accessToken || !refreshToken) {
+    if (!accessToken || !refreshToken) {
+      cleanCookies(cookies);
+      // cleanStores($cart);
+      // $cart.set(null);
+      console.log("no tokens");
+      if (micromatch.isMatch(url.pathname, protectedRoutes)) {
         return redirect("/signin");
       }
-      
+    }
+    else if(accessToken && refreshToken) {
       const { data, error } = await supabase.auth.setSession({
         refresh_token: refreshToken.value,
         access_token: accessToken.value,
       });
     
       if (error) {
-        cookies.delete("sb-access-token", {
-          path: "/",
-        });
-        cookies.delete("sb-refresh-token", {
-          path: "/",
-        });
+        cleanCookies(cookies);
+        // cleanStores($cart);
         return redirect("/signin");
       }
 
-      locals.email = data.user?.email!;
+      console.log(locals);
       cookies.set("sb-access-token", data?.session?.access_token!, {
         sameSite: "strict",
         path: "/",
