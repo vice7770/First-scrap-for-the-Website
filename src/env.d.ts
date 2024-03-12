0d5fe1263bdfbb8ec6183253a01e33168bd9e7da
vicente almeida <vicente.cra45@gmail.com>
2024-03-13 00:36:48 +0400

    Cloudinary and keys

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,19 @@
 interface ImportMetaEnv {
     readonly SUPABASE_URL: string
     readonly SUPABASE_ANON_KEY: string
+    readonly CLOUDINARY_CLOUD_NAME: string
+    readonly CLOUDINARY_API_KEY: string
+    readonly CLOUDINARY_API_SECRET: string
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+
+declare namespace App {
+  interface Locals {
+    user: {
+        email: string
+    }
+  }
 }
