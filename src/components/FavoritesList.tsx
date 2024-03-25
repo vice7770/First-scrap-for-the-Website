230b86401ed1fe208ebec81f2ae2d0de78c9663b
vicente almeida <vicente.cra45@gmail.com>
2024-03-26 01:39:53 +0400

    Redo of favorites to fetch on server side

--- src/components/FavoritesList.tsx
+++ src/components/FavoritesList.tsx
@@ -3,17 +3,32 @@ import { Skeleton } from "./ui/skeleton";
 import { $favorites, getFavorites } from "@/stores/favorites";
 import { useStore } from "@nanostores/react";
 import { $isFavoritesFetched } from "@/stores/isDataFetched";
+import type { shopResult } from "@/utils/schemas";
+import type { z } from "zod";
 
-function FavoritesList({favorites}: {favorites: any}) {
+import { cld } from "@/consts";
+import { format, quality } from "@cloudinary/url-gen/actions/delivery";
+import { scale } from "@cloudinary/url-gen/actions/resize";
+import { auto } from "@cloudinary/url-gen/qualifiers/format";
+
+
+type ShopData = z.infer<typeof shopResult>;
+
+function FavoritesList({favorites}: {favorites: ShopData}) {
+    const urlImages = favorites?.map((favorite) => {
+        const image = cld.image(favorite.public_id).delivery(quality('auto:eco')).delivery(format(auto()));
+        image.resize(scale().width(650).height(650).aspectRatio("1.0"));
+        return image.toURL();
+    });
     return(
         <ul className="flex flex-wrap w-full h-full">
-            {favorites?.map((post, index) => (
+            {favorites?.map((favorite, index) => (
                 <li key={index} className="w-1/3 px-2">
-                    <a href={`/shop/${post.slug}/`}>
-                        <img className="w-full h-auto" draggable='false' height={613} width={613} src={post.data.image} alt="" 
+                    <a href={`/shop/${favorite.id}/`}>
+                        <img className="w-full h-auto" draggable='false' height={613} width={613} src={urlImages[index]} alt="" 
                     />
                     <h4 className="flex content-center justify-center title p-4">       
-                        {post.data.title}
+                        {favorite.name}
                     </h4>
                 </a>
             </li>
@@ -22,11 +37,10 @@ function FavoritesList({favorites}: {favorites: any}) {
     )
 }
 
-function PostList({favorites}: {favorites: any}) {
+function PostList({favorites}: {favorites: ShopData}) {
     // const [isLoading, setIsLoading] = useState(true);
     const favoriteIds = useStore($favorites);
     const isFavoritesFetched = useStore($isFavoritesFetched);
-    const filteredFavorites = favorites.filter((post: any) => favoriteIds?.includes(post.data.id));
     useEffect(() => {
         if(!isFavoritesFetched){
             getFavorites();
@@ -35,7 +49,7 @@ function PostList({favorites}: {favorites: any}) {
         // setIsLoading(false);
     }, []);
     return (
-        filteredFavorites.length > 0 ? <FavoritesList favorites={filteredFavorites} /> : <p className="flex items-center justify-center text-6xl w-full min-h-96">No items</p>
+        favoriteIds && favoriteIds.length > 0 ? <FavoritesList favorites={favorites} /> : <p className="flex items-center justify-center text-6xl w-full min-h-96">No items</p>
     )
 }
 
---
+++ src/pages/api/favoritesFromShop.ts
@@ -0,0 +1,47 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../lib/supabase";
+
+export const GET: APIRoute = async ({ request }) => {
+  const { data: userData } = await supabase.auth.getUser();
+  const userId = userData?.user?.id;
+
+  if (!userId) {
+    return new Response(
+      JSON.stringify({
+        error: "User not authenticated",
+      }),
+      { status: 401 },
+    );
+  }
+
+  const { data: favoritesData, error: favoritesError } = await supabase
+    .from('favorites')
+    .select('item_id')
+    .eq('user_id', userId);
+
+  if (favoritesError || !Array.isArray(favoritesData)) {
+    return new Response(
+      JSON.stringify({
+        error: favoritesError?.message || "Invalid data",
+      }),
+      { status: 500 },
+    );
+  }
+
+  const arrayOfIds = favoritesData.map((item: any) => item.item_id);
+  const { data, error } = await supabase
+    .from('shop')
+    .select('*')
+    .in('id', arrayOfIds);
+
+  if (error || !Array.isArray(data)) {
+    return new Response(
+      JSON.stringify({
+        error: error?.message || "Invalid data",
+      }),
+      { status: 500 },
+    );
+  }
+
+  return new Response(JSON.stringify(data));
+}
