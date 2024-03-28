f55ae85a8185dc049c060fc3d37b0790113ffd82
vicente almeida <vicente.cra45@gmail.com>
2024-03-28 18:24:19 +0400

    changes to carousel

--- src/components/ui/carouselHomeItems.tsx
+++ src/components/ui/carouselHomeItems.tsx
@@ -8,15 +8,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import type { ShopData } from "@/utils/schemas"
 
-export function CarouselHomeItem({ trendItems }: { trendItems: any }) {
+import { cld } from "@/consts";
+import { format, quality } from "@cloudinary/url-gen/actions/delivery";
+import { scale } from "@cloudinary/url-gen/actions/resize";
+import { auto } from "@cloudinary/url-gen/qualifiers/format";
+
+export function CarouselHomeItem({ trendItems }: { trendItems: ShopData }) {
+  const urlImages = trendItems?.map((item) => {
+    const image = cld.image(item.public_id).delivery(quality('auto:eco')).delivery(format(auto()));
+    image.resize(scale().width(650).height(650).aspectRatio("1.0"));
+    return image.toURL();
+});
   return (
     <Carousel
       opts={{
         align: "start",
         loop: true,
       }}
-      
+
     >
       <CarouselContent>
         {trendItems?.map((item: any, index: React.Key | null | undefined) => (
@@ -24,11 +35,11 @@ export function CarouselHomeItem({ trendItems }: { trendItems: any }) {
             <div className="p-1">
               <Card className="rounded-none">
                 <CardContent className="flex aspect-square items-center justify-center p-0 ">
-                <a href={`/shop/${item.slug}/`}>
+                <a href={`/shop/${item.id}/`}>
                 <div className="relative">
-                  <img src={item.data.image} alt={item.data.title} className="w-full" />
+                  <img src={urlImages[index]} draggable='false' alt={item.name} className="w-full" />
                   <h4 className="absolute top-0 left-0 p-4 bg-transparent text-black flex items-center justify-center">
-                    {item.data.title}
+                    {item.name}
                   </h4>
                 </div>
                 </a>
