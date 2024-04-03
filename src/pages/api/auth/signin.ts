7368643066da031aeacbe50bf9fe15c5455e0572
vicente almeida <vicente.cra45@gmail.com>
2024-04-04 01:23:08 +0400

    Removing path from cookie

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -47,10 +47,12 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   const { access_token, refresh_token } = data.session;
   cookies.set("sb-access-token", access_token, {
     sameSite: "strict",
+    path: redirectUrl || "/",
     secure: true,
   });
   cookies.set("sb-refresh-token", refresh_token, {
     sameSite: "strict",
+    path: redirectUrl || "/",
     secure: true,
   });
 
